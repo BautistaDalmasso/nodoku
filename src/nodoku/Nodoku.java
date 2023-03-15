76d765f22139597c9e2b7f6f6568a6d2cb817397
Bautista Dalmasso <j.bauti.d@gmail.com>
2023-03-15 12:58:58 -0300

    Crea clase Nodoku con funcionalidad basica para generar juegos.

---
+++ src/nodoku/Nodoku.java
@@ -0,0 +1,37 @@
+package nodoku;
+
+import java.util.Random;
+
+public class Nodoku {
+	int anchoGrilla;
+	int largoGrilla;
+	int valorMaximoCelda;
+	int[] sumasEsperadasPorFila;
+	int[] sumasEsperadasPorColumna;
+	
+	private void generarJuego()
+	{
+		/* Genera las listas de sumas esperadas de modo que el juego tenga solución. */
+		Random rand = new Random();
+		
+		this.sumasEsperadasPorColumna = Nodoku.instanciarArregloConCeros(anchoGrilla);
+		this.sumasEsperadasPorFila = Nodoku.instanciarArregloConCeros(largoGrilla);
+		
+		for(int columna=0; columna<anchoGrilla; columna++) {
+			for(int fila=0; fila<largoGrilla; fila++) {
+				int numeroCelda = rand.nextInt(1, valorMaximoCelda+1);
+				
+				sumasEsperadasPorColumna[columna] += numeroCelda;
+				sumasEsperadasPorFila[fila] += numeroCelda;
+			}
+		}
+	}
+	private static int[] instanciarArregloConCeros(int tamanio) {
+		int[] nuevoArreglo = new int[tamanio];
+		
+		for (int i = 0; i < tamanio; i++) {
+			nuevoArreglo[i] = 0;
+		}
+		return nuevoArreglo;
+	}
+}
