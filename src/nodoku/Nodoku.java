7a4a6dcf5e6a079cb246dd5896349084c079eced
Bautista Dalmasso <j.bauti.d@gmail.com>
2023-03-19 19:41:41 -0300

    Crea un constructor basico para Nodoku.

--- src/nodoku/Nodoku.java
+++ src/nodoku/Nodoku.java
@@ -9,6 +9,14 @@ public class Nodoku {
 	int[] sumasEsperadasPorFila;
 	int[] sumasEsperadasPorColumna;
 	
+	public Nodoku(int tamanio) {
+		/* Crea un tablero de Nodoku cuadrado de tamanio x tamanio. */
+		anchoGrilla = tamanio;
+		largoGrilla = tamanio;
+		
+		generarJuego();
+	}
+	
 	private void generarJuego()
 	{
 		/* Genera las listas de sumas esperadas de modo que el juego tenga solución. */
