6b30e52be3210ef8e01eb46856f715ba01b32c16
Bautista Dalmasso <j.bauti.d@gmail.com>
2023-03-22 17:42:53 -0300

    Permite guardar el estado del Ranking a un archivo.

--- src/nodoku/Ranking.java
+++ src/nodoku/Ranking.java
@@ -2,6 +2,7 @@ package nodoku;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.LinkedList;
@@ -14,20 +15,51 @@ public class Ranking {
 	Map<Long, String> jugadoresEnElRankingPorTiempo;
 	List<Long> mejoresTiempos;
 	
-	public Ranking() throws IOException {
+	public Ranking() {
 		archivoRankings = new File("rankings.txt");
 		
-		boolean archivoCreado = archivoRankings.createNewFile();
+		boolean archivoCreado;
+		try {
+			archivoCreado = archivoRankings.createNewFile();
+			if (archivoCreado) {
+				// El archivo no existia previamente.
+				jugadoresEnElRankingPorTiempo = new HashMap<Long, String>();
+				mejoresTiempos = new LinkedList<Long>();
+			}
+			else {
+				// Ya existe un archivo con los rankings.
+				leerRankings();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void agregarJugador(Long tiempo, String nombre) {
+		jugadoresEnElRankingPorTiempo.put(tiempo, nombre);
+		mejoresTiempos.add(tiempo);
+	}
+	
+	public void guardarRanking() {
+		try {
+			FileWriter fw = new FileWriter("rankings.txt");
+			
+			mejoresTiempos.sort(null);
+			StringBuilder rankingAGuardar = new StringBuilder();
+			for (int i=0; i<10 && i<mejoresTiempos.size(); i++) {
+				// Solo guardamos los mejores 10 tiempos.
+				Long tiempoDeCompletacion = mejoresTiempos.get(i);
+				rankingAGuardar.append(tiempoDeCompletacion.toString() 
+						+ "," + jugadoresEnElRankingPorTiempo.get(tiempoDeCompletacion) 
+						+ "\n");
+			}
+			fw.write(rankingAGuardar.toString());
+			fw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
-		if (archivoCreado) {
-			// El archivo no existia previamente.
-			jugadoresEnElRankingPorTiempo = new HashMap<Long, String>();
-			mejoresTiempos = new LinkedList<Long>();
-		}
-		else {
-			// Ya existe un archivo con los rankings.
-			leerRankings();
-		}
+		
 	}
 
 	private void leerRankings() throws IOException {
@@ -38,8 +70,7 @@ public class Ranking {
 		
 		while (s.hasNextLine()) {
 			Jugador j = new Jugador(s.nextLine().split(","));
-			jugadoresEnElRankingPorTiempo.put(j.tiempoDeCompletación, j.nombre);
-			mejoresTiempos.add(j.tiempoDeCompletación);
+			agregarJugador(j.tiempoDeCompletación, j.nombre);
 		}
 	}
 }
