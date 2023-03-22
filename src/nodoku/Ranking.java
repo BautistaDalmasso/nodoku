f137a4e3d0e08b1f3ba139b7c394810935753c4c
Bautista Dalmasso <j.bauti.d@gmail.com>
2023-03-22 17:27:37 -0300

    Permite a ranking leer un archivo con rankings.

--- src/nodoku/Ranking.java
+++ src/nodoku/Ranking.java
@@ -1,11 +1,13 @@
 package nodoku;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 
 public class Ranking {
 	File archivoRankings;
@@ -28,8 +30,29 @@ public class Ranking {
 		}
 	}
 
-	private void leerRankings() {
-		// TODO Auto-generated method stub
+	private void leerRankings() throws IOException {
+		/* Los rankings se guardan en un archivo donde cada línea sigue el 
+		 * siguiente formato:
+		 * <tiempo_de_completacion_en_milisegundos>,<nombre_jugador>*/
+		Scanner s = new Scanner(archivoRankings);
 		
+		while (s.hasNextLine()) {
+			Jugador j = new Jugador(s.nextLine().split(","));
+			jugadoresEnElRankingPorTiempo.put(j.tiempoDeCompletación, j.nombre);
+			mejoresTiempos.add(j.tiempoDeCompletación);
+		}
+	}
+}
+
+class Jugador {
+	Long tiempoDeCompletación;
+	String nombre;
+	
+	int TIEMPO = 0;
+	int NOMBRE = 1;
+	
+	public Jugador(String[] tiempoYJugador) {
+		tiempoDeCompletación = Long.parseLong(tiempoYJugador[TIEMPO]);
+		nombre = tiempoYJugador[NOMBRE];
 	}
 }
